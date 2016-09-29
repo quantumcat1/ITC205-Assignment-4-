c9a5fa75253458b174384a875c94c1e4dcd5aa45
Erin <thequantumcat@hotmail.com>
2016-09-29 17:52:09 +1000

    adds replication for Bug 4
    Bug 4 can be replicated but has not yet been formally investigated. Some
    things have been added to Main which cuts out uninteresting status
    messages from the output if we are testing for Bug 4. NO logic has
    changed.

--- src/game/Main.java
+++ src/game/Main.java
@@ -7,6 +7,11 @@ public class Main
 {
 	public static void main(String[] args) throws Exception
 	{
+		boolean bTestBug4 = false;
+		if(args[0] != null)
+		{
+			bTestBug4 = true;
+		}
 		BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
 
         Dice d1 = new Dice();
@@ -34,9 +39,12 @@ public class Main
                 player.setLimit(limit);
                 int bet = 5;
 
-                System.out.println(String.format("Start Game %d: ", i));
-                System.out.println(String.format("%s starts with balance %d, limit %d",
-                		player.getName(), player.getBalance(), player.getLimit()));
+                if(!bTestBug4)
+                {
+	                System.out.println(String.format("Start Game %d: ", i));
+	                System.out.println(String.format("%s starts with balance %d, limit %d",
+	                		player.getName(), player.getBalance(), player.getLimit()));
+                }
 
                 int turn = 0;
                 while (player.balanceExceedsLimitBy(bet) && player.getBalance() < 200)
@@ -44,8 +52,11 @@ public class Main
                     turn++;
                 	DiceValue pick = DiceValue.getRandom();
 
-                	System.out.printf("Turn %d: %s bet %d on %s\n",
-                			turn, player.getName(), bet, pick);
+                	if(!bTestBug4)
+                	{
+	                	System.out.printf("Turn %d: %s bet %d on %s\n",
+	                			turn, player.getName(), bet, pick);
+                	}
 
                 	int winnings = game.playRound(player, pick, bet);
                     cdv = game.getDiceValues();
@@ -53,25 +64,37 @@ public class Main
                     System.out.printf("Rolled %s, %s, %s\n",
                     		cdv.get(0), cdv.get(1), cdv.get(2));
 
-                    if (winnings > 0) {
-	                    System.out.printf("%s won %d, balance now %d\n\n",
-	                    		player.getName(), winnings, player.getBalance());
+                    if (winnings > 0)
+                    {
+                    	if(!bTestBug4)
+                    	{
+		                    System.out.printf("%s won %d, balance now %d\n\n",
+		                    		player.getName(), winnings, player.getBalance());
+                    	}
 	                	winCount++;
                     }
-                    else {
-	                    System.out.printf("%s lost, balance now %d\n\n",
-	                    		player.getName(), player.getBalance());
+                    else
+                    {
+                    	if(!bTestBug4)
+                    	{
+		                    System.out.printf("%s lost, balance now %d\n\n",
+		                    		player.getName(), player.getBalance());
+                    	}
 	                	loseCount++;
                     }
 
                 } //while
-
-                System.out.print(String.format("%d turns later.\nEnd Game %d: ", turn, i));
-                System.out.println(String.format("%s now has balance %d\n", player.getName(), player.getBalance()));
-
+                if(!bTestBug4)
+                {
+                	System.out.print(String.format("%d turns later.\nEnd Game %d: ", turn, i));
+                	System.out.println(String.format("%s now has balance %d\n", player.getName(), player.getBalance()));
+                }
             } //for
 
-            System.out.println(String.format("Win count = %d, Lose Count = %d, %.2f", winCount, loseCount, (float) winCount/(winCount+loseCount)));
+            if(!bTestBug4)
+            {
+            	System.out.println(String.format("Win count = %d, Lose Count = %d, %.2f", winCount, loseCount, (float) winCount/(winCount+loseCount)));
+            }
             totalWins += winCount;
             totalLosses += loseCount;
 
@@ -79,7 +102,9 @@ public class Main
             if (ans.equals("q")) break;
         } //while true
 
-        System.out.println(String.format("Overall win rate = %.1f%%", (float)(totalWins * 100) / (totalWins + totalLosses)));
+        if(!bTestBug4)
+        {
+        	System.out.println(String.format("Overall win rate = %.1f%%", (float)(totalWins * 100) / (totalWins + totalLosses)));
+        }
 	}
-
 }
---
+++ src/replication/Bug4Replication.java
@@ -0,0 +1,12 @@
+package replication;
+
+import game.Main;
+
+public class Bug4Replication
+{
+	public static void main(String[] args) throws Exception
+	{
+		String[] argsTest = {"t"};
+		Main.main(argsTest);
+	}
+}
