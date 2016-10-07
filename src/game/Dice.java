3bfc918e63284c7d6394e7aea379e9b9a095df51
Erin <thequantumcat@hotmail.com>
2016-10-07 16:20:39 +1100

    bug 4 resolution
    I think it didn;t get committed before (I reverted some commits because I put in the wrong files or had the wrong name, and Dice.java may have gotten left out

--- src/game/Dice.java
+++ src/game/Dice.java
@@ -14,15 +14,10 @@ public class Dice
 		return value;
 	}
 
-	/*possible defect: the return value does not get
-	 * captured. (i.e. it doesn't set 'value' to
-	 * what it gets from the getRandom function).
-	 * In Game.playRound, the dice is just rolled
-	 * but the result is not captured.
-	 */
 	public DiceValue roll()
 	{
-		return DiceValue.getRandom();
+		value = DiceValue.getRandom();
+		return value;
 	}
 
 	public String toString()
